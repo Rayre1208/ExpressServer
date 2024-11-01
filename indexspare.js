08f4acbd2cfb71b1ae7d6ebe9466eae3c872a350
Wistorian <eazersupplicate@gmail.com>
2024-11-01 08:43:44 +0800

    2024/11/1_0845

--- indexspare.js
+++ indexspare.js
@@ -1,18 +1,16 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// 引入 response.js 中的 JSON 資料
-const responseData = require('./response.js');
-
-app.get('/', (req, res) => {
-  res.json(responseData); // 將 responseData 作為 JSON 格式回應
+app.get("/", (req, res) => {
+  res.json({
+    message: "test work newest D ver!!",
+  });
 });
 
-
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`Express Server started on port ${port}`);

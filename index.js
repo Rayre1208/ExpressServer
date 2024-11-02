5a0a21b025dcf60d26e6ba02428326b75e586965
Wistorian <eazersupplicate@gmail.com>
2024-11-02 19:30:23 +0800

    241102_1930

--- index.js
+++ index.js
@@ -10,7 +10,16 @@ const responseData = require('./response.js');
 
 app.get('/', (req, res) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:8084');
-  res.json(responseData); // 將 responseData 作為 JSON 格式回應
+
+     // 獲取查詢參數，並轉換為小寫，方便進行不區分大小寫的搜尋
+  const query = req.query.query?.toLowerCase();
+
+  // 篩選 products 陣列，只保留 randomtutors.name.full 包含查詢關鍵字的物件
+  const filteredProducts = responseData.products.filter(product => {
+    return product.randomtutors.name.full.toLowerCase().includes(query);
+  });
+
+  res.json({ products: filteredProducts });
 });
 
 
